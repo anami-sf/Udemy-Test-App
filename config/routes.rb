ab99cc6317d41c3af9f7a88ad092af3b27bc179c
Ana <anami823@gmail.com>
2018-12-05 14:55:13 -0800

    Added 'pg' gem to production in gemfile.

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :articles
+  get 'welcome/home', to: 'welcome#home'
+
+  get 'welcome/about', to: 'welcome#about'
+
+  #root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
